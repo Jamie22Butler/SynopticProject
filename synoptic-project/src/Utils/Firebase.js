8a320807fed72a4c3fef90fee38535125a257bf9
Jamie22Butler <78593709+Jamie22Butler@users.noreply.github.com>
2021-10-18 16:09:24 +0100

    Made connection to Firestore database

---
+++ synoptic-project/src/Utils/Firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVq0s5hLsHk6d6nSx8gMp0aS69wKZXlN0",
+  authDomain: "synopticproject-d3bb9.firebaseapp.com",
+  projectId: "synopticproject-d3bb9",
+  storageBucket: "synopticproject-d3bb9.appspot.com",
+  messagingSenderId: "644298469137",
+  appId: "1:644298469137:web:ca1ec137be74b30d1926b0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
