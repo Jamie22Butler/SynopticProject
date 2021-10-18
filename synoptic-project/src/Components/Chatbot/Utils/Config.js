dd22100e6ee25370a4a6abfbc5b48c14315f8348
Jamie22Butler <78593709+Jamie22Butler@users.noreply.github.com>
2021-10-18 10:54:23 +0100

    Initial Config created

---
+++ synoptic-project/src/Components/Chatbot/Utils/Config.js
@@ -0,0 +1,15 @@
+import { createChatBotMessage } from 'react-chatbot-kit'
+
+const botName = 'Holiday Bot'
+const config = {
+    botName: botName,
+    initialMessage: [
+        createChatBotMessage(`Hey there I'm ${botName}!`),
+        createChatBotMessage(`How can I help?`, {
+            // Delay for cosmetic purposes in the render of the initial messages.
+            delay: 1250
+        })
+    ] 
+};
+
+export default config;
